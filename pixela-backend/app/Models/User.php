7639bc00563efbed2b6e7e3d0267c3688662074f
envyx10 <pabloinfodesign95@gmail.com>
2025-04-08 21:04:33 +0200

    refactor: rename Users model to User for Laravel convention

--- pixela-backend/app/Models/User.php
+++ pixela-backend/app/Models/User.php
@@ -8,12 +8,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class Users extends Authenticatable
+class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens, HasFactory, Notifiable;
-
-    protected $table = 'users';
+    
     protected $primaryKey = 'user_id';
 
 
