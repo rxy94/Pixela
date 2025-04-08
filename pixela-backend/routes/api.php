e98d5bcdca138473637cac448468be9d3cfe9483
rxy94 <ruyi1994@gmail.com>
2025-04-08 19:38:36 +0200

    refactor: Reorganización de rutas API para películas y series
    - Agrupación de rutas de películas bajo el prefijo 'movies'
    - Agrupación de rutas de series bajo el prefijo 'series'
    - Mejora en la estructura y legibilidad del archivo de rutas

--- pixela-backend/routes/api.php
+++ pixela-backend/routes/api.php
@@ -15,19 +15,27 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 // Rutas de películas
-Route::group(['controller' => MovieController::class], function () {
-    Route::get('/movie/{movieId}', 'getMovieDetails'); #http GET http://localhost/api/movie/550
-    Route::get('/movies/popular', 'getAllPopularMovies'); #http GET http://localhost/api/movies/popular
-    Route::get('/movies/genre/{genreId}', 'getMovieByGenre'); #http GET http://localhost/api/movies/genre/28
-    Route::get('/movies/now-playing', 'getMovieNowPlaying'); #http GET http://localhost/api/movies/now-playing
-    Route::get('/movies/top-rated', 'getTopRatedMovies'); #http GET http://localhost/api/movies/top-rated
+Route::group([
+    'controller' => MovieController::class, 
+    'prefix' => 'movies'], function () {
+
+    Route::get('/{movieId}', 'getMovieDetails'); #http GET http://localhost/api/movies/550
+    Route::get('/popular', 'getAllPopularMovies'); #http GET http://localhost/api/movies/popular
+    Route::get('/genre/{genreId}', 'getMovieByGenre'); #http GET http://localhost/api/movies/genre/28
+    Route::get('/now-playing', 'getMovieNowPlaying'); #http GET http://localhost/api/movies/now-playing
+    Route::get('/top-rated', 'getTopRatedMovies'); #http GET http://localhost/api/movies/top-rated
+
 });
 
 // Rutas de series
-Route::group(['controller' => SeriesController::class], function () {
-    Route::get('/series/{seriesId}', 'getSeriesDetails')->where('seriesId', '[0-9]+'); #http GET http://localhost/api/series/108978
-    Route::get('/series/popular', 'getAllPopularSeries'); #http GET http://localhost/api/series/popular
-    Route::get('/series/genre/{genreId}', 'getSeriesByGenre'); #http GET http://localhost/api/series/genre/10759
-    Route::get('/series/on-the-air', 'getSeriesOnTheAir'); #http GET http://localhost/api/series/on-the-air
-    Route::get('/series/top-rated', 'getTopRatedSeries'); #http GET http://localhost/api/series/top-rated
+Route::group([
+    'controller' => SeriesController::class,
+    'prefix' => 'series'], function () {
+
+    Route::get('/{seriesId}', 'getSeriesDetails')->where('seriesId', '[0-9]+'); #http GET http://localhost/api/series/108978
+    Route::get('/popular', 'getAllPopularSeries'); #http GET http://localhost/api/series/popular
+    Route::get('/genre/{genreId}', 'getSeriesByGenre'); #http GET http://localhost/api/series/genre/10759
+    Route::get('/on-the-air', 'getSeriesOnTheAir'); #http GET http://localhost/api/series/on-the-air
+    Route::get('/top-rated', 'getTopRatedSeries'); #http GET http://localhost/api/series/top-rated
+    
 });
