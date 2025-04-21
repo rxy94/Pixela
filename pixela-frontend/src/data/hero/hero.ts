dbb95b46d7511d6eb6bf21acdcabb60af7e68935
envyx10 <pabloinfodesign95@gmail.com>
2025-04-21 18:30:51 +0200

    Agregar: nuevas imágenes a la sección Hero y actualizar la lista de imágenes en los archivos correspondientes.

--- pixela-frontend/src/data/hero/hero.ts
+++ pixela-frontend/src/data/hero/hero.ts
@@ -3,8 +3,11 @@ export const heroData = {
   accentTitle: "cinematográfico",
   description: "Descubre, colecciona y comparte experiencias audiovisuales en una comunidad de apasionados del cine y las series.",
   secondaryButtonText: "Descubrir más",
-  images: ["/source/images/Adolescence.avif", 
-    "/source/images/Blackmirror.avif", 
+  images: [
+    "/source/images/iamovie.avif",
+    "/source/images/iamovie2.webp",
+    "/source/images/Adolescence.avif", 
     "/source/images/MovieDestacada_uno.avif", 
-    "/source/images/MovieDestacada_dos.avif"],
+    "/source/images/prueba6.jpg",
+  ],
 };
