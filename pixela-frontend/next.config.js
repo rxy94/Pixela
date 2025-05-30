f342bc935a8606d2d6c8146a693f065175f3bb1a
rxy94 <ruyi1994@gmail.com>
2025-05-30 18:02:39 +0200

    refactor: añadir variables de entorno en docker-compose.yml

--- pixela-frontend/next.config.js
+++ pixela-frontend/next.config.js
@@ -1,16 +1,20 @@
 /** @type {import('next').NextConfig} */
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+// Para despliegue --> const backendApiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost/api';
+
 const nextConfig = {
     async rewrites() {
         return [
             {
                 source: '/api/series/:path*',
                 destination: 'http://localhost/api/series/:path*',
+                // Despliegue --> destination: `${backendApiUrl}/series/:path*`,
             },
             {
                 source: '/api/movies/:path*',
                 destination: 'http://localhost/api/movies/:path*',
+                // Despliegue --> destination: `${backendApiUrl}/movies/:path*`,
             },
         ];
     },
