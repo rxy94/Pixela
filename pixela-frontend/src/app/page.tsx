d567ab3edd533dd2ac310043afadb535ee200fc9
envyx10 <pabloinfodesign95@gmail.com>
2025-06-08 20:49:10 +0200

    fix(home): eliminar encabezado aleatorio en la sección de descubrimiento
    - Se eliminó la importación y uso del encabezado aleatorio en el componente `Home`, simplificando la sección de descubrimiento.
    - Este cambio mejora la claridad y funcionalidad del código en la página principal.

--- pixela-frontend/src/app/page.tsx
+++ pixela-frontend/src/app/page.tsx
@@ -6,8 +6,6 @@ import { getTrendingSeries, getTrendingMovies } from "@/features/trending/servic
 import { getDiscoveredSeries, getDiscoveredMovies } from "@/features/discover/service";
 import AboutSection from "@/features/about/components/AboutSection";
 import { getRandomQuote } from "@/features/quotes/service";
-import { getRandomHeading } from "@/features/discover/service/headingService";
-
 
 export const dynamic = 'force-dynamic';
 
@@ -33,7 +31,7 @@ export default async function Home() {
   ]);
 
   const randomQuote = getRandomQuote();
-  const randomHeading = getRandomHeading();
+
 
   return (
     <main className={STYLES.main}>
@@ -42,7 +40,7 @@ export default async function Home() {
         <TrendingSection series={trendingSeries} movies={trendingMovies} quote={randomQuote} />
       </div>
       <div id="discover" className={STYLES.section}>
-        <DiscoverSection series={discoveredSeries} movies={discoveredMovies} heading={randomHeading} />
+        <DiscoverSection series={discoveredSeries} movies={discoveredMovies} />
       </div>
       <div id="about" className={STYLES.section}>
         <AboutSection />
