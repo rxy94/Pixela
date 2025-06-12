ffda0d924873b9c02dee1a99d6bdee3776fe2b3b
envyx10 <pabloinfodesign95@gmail.com>
2025-06-12 22:10:44 +0200

    feat: actualizar la lógica de puntuación en componentes de reseñas
    - Se modificó la visualización de la puntuación en ReviewModal, StarDisplay y ProfileReviews para reflejar una escala de 0-5 en lugar de 0-10, mejorando la claridad de la calificación.
    - Se ajustó el cálculo de la puntuación en los componentes para asegurar una conversión adecuada de la escala.

--- pixela-frontend/src/features/media/components/review/StarDisplay.tsx
+++ pixela-frontend/src/features/media/components/review/StarDisplay.tsx
@@ -14,29 +14,33 @@ const STYLES = {
  * @param {StarDisplayProps} props - Propiedades del componente
  * @returns {JSX.Element} Componente de puntuación de la reseña
  */
-export const StarDisplay = ({ value }: StarDisplayProps) => (
-  <span className={STYLES.container}>
-    {[1, 2, 3, 4, 5].map((star) => {
-      const starValue = star * 2;
-      const isFull = value >= starValue;
-      const isHalf = value === starValue - 1;
-      return (
-        <span
-          key={star}
-          className={STYLES.star}
-        >
-          <FiStar className={`w-6 h-6 absolute top-0 left-0 ${isFull ? 'text-yellow-400' : 'text-gray-400'}`} />
-          {isHalf && (
-            <FiStar
-              className="absolute top-0 left-0 w-6 h-6 text-yellow-400"
-              style={{ clipPath: 'inset(0 50% 0 0)' }}
-            />
-          )}
-        </span>
-      );
-    })}
-    <span className={STYLES.value}>
-      {value % 1 === 0 ? value : value.toFixed(1)}/10
+export const StarDisplay = ({ value }: StarDisplayProps) => {
+  // Convertir la puntuación de escala 0-10 a escala 0-5
+  const valueOn5Scale = value / 2;
+  
+  return (
+    <span className={STYLES.container}>
+      {[1, 2, 3, 4, 5].map((star) => {
+        const isFull = valueOn5Scale >= star;
+        const isHalf = !isFull && valueOn5Scale >= star - 0.5;
+        return (
+          <span
+            key={star}
+            className={STYLES.star}
+          >
+            <FiStar className={`w-6 h-6 absolute top-0 left-0 ${isFull ? 'text-yellow-400' : 'text-gray-400'}`} />
+            {isHalf && (
+              <FiStar
+                className="absolute top-0 left-0 w-6 h-6 text-yellow-400"
+                style={{ clipPath: 'inset(0 50% 0 0)' }}
+              />
+            )}
+          </span>
+        );
+      })}
+      <span className={STYLES.value}>
+        {valueOn5Scale % 1 === 0 ? valueOn5Scale : valueOn5Scale.toFixed(1)}/5
+      </span>
     </span>
-  </span>
-); 
+  );
+}; 
